8aef1aae9f718426caa561f4a3813d35b9d3475c
Jöbstl Stefan <stefan.joebstl@edu.fh-joanneum.at>
2024-01-26 16:00:06 +0100

    Added azure

--- TaskHub/settings.py
+++ TaskHub/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import datetime
+import os
 from pathlib import Path
 from decouple import config
 
@@ -73,16 +74,34 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'TaskHub.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.environ.get('USE_AZURE_DB') == "true":
+    DATABASES = {
+      'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'taskhub',
+        'USER': os.environ.get('DB_user'),
+        'PASSWORD': os.environ.get('DB_password'),
+        'HOST': 'wapdev2.postgres.database.azure.com',   # Or an IP Address that your DB is hosted on
+        'PORT': '5432',
+        },
     }
-}
+else:
+    DATABASES = {
+      'default': {
+          'ENGINE': 'django.db.backends.sqlite3',
+          'NAME': BASE_DIR / 'db.sqlite3',
+      }
+    }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 
 # Password validation
